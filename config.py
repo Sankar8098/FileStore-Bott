7943a509ddb5895be356f9677cb3c0c901f57159
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2025-01-23 17:25:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "5821871362"))
 PORT = os.environ.get("PORT", "8080")
 
 #File Auto Delete
-FILE_AUTO_DELETE = int(os.environ.get("FILE_AUTO_DELETE", "45")) # auto delete in seconds
+FILE_AUTO_DELETE = int(os.environ.get("FILE_AUTO_DELETE", "86200")) # auto delete in seconds
 
 
 #Database 
